1de55edef4b6f059d6854f1071497305b00e3172
wangle <251668577@qq.com>
2019-03-07 12:21:04 +0800

    添加Git3.java

---
+++ java8/src/main/java/com/lee/java8/Git3.java
@@ -0,0 +1,12 @@
+package com.lee.java8;
+
+/**
+ * @author WangLe
+ * @date 2019/3/7 12:20
+ * @description
+ */
+public class Git3 {
+    public static void main(String[] args) {
+        System.out.println("环境搜房骄傲so");
+    }
+}
