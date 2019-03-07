eefc0d1999cde71c21113dfed816a53ff465616f
wangle <251668577@qq.com>
2019-03-07 12:13:59 +0800

    删除Git2.java22222

--- java8/src/main/java/com/lee/java8/Git2.java
+++ java8/src/main/java/com/lee/java8/Git2.java
@@ -8,5 +8,6 @@ package com.lee.java8;
 public class Git2 {
     public static void main(String[] args) {
         System.out.println("发撒开了房间爱搜ID房间爱搜ID房间爱搜i");
+        System.out.println("哈哈哈哈");
     }
 }
