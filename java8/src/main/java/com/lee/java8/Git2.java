a5d01773b3cc32b72f08dca4bedc5db7da48c103
wangle <251668577@qq.com>
2019-03-07 12:17:33 +0800

    更改Git2.java111

--- java8/src/main/java/com/lee/java8/Git2.java
+++ java8/src/main/java/com/lee/java8/Git2.java
@@ -7,7 +7,6 @@ package com.lee.java8;
  */
 public class Git2 {
     public static void main(String[] args) {
-        System.out.println("发撒开了房间爱搜ID房间爱搜ID房间爱搜i");
         System.out.println("哈哈哈哈");
         System.out.println("是金佛阿斯加防冻年少");
     }
